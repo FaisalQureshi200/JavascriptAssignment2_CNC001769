957b582e50beb116467ccd127eccbbac5dccb4b3
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-14 08:23:54 +0500

    This is Task8 file

---
+++ Task8/Task8.js
@@ -0,0 +1,10 @@
+var number=Number(prompt('Enter Number'))
+if(number>=0){
+    alert('Number is postive')
+}
+else if(number<0){
+    alert('Number is negative')
+}
+else {
+    alert('you enter wrong number')
+}
