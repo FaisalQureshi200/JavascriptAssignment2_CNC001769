5bd32cb8a90fd5c575a29fd9182ebc086a4bb2cf
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-14 08:08:16 +0500

    This is Task2 file

---
+++ Task2/Task2.js
@@ -0,0 +1,16 @@
+var signallight=prompt('enter color of traffic signal')
+if(signallight=="red"){
+alert('Must Stop')
+}
+if(signallight=="yellow")
+{
+alert('Ready to move')
+}
+if(signallight=="green")
+{
+alert('Move Now')
+}
+else{
+    alert("Please enter correct color")
+}
+
